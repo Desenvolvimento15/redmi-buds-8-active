637de340687d9ee39d45eee73fc54a040413c956
H3110 <96912707+Desenvolvimento15@users.noreply.github.com>
2026-05-21 11:20:04 -0300

    vs01

---
+++ main.js
@@ -0,0 +1,10 @@
+$(".slick-one").slick({
+    lazyLoad: "ondemand",
+    infinite: true,
+    fade: true,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    speed: 500,
+    cssEase: 'linear'
+});
